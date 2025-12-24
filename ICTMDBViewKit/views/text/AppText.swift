1ab017305d0a7e3c463887b29d2723cc0d37bd82
Engin Gülek <74055938+engingulek@users.noreply.github.com>
2025-12-24 20:11:59 +0300

    Created AppText

---
+++ ICTMDBViewKit/views/text/AppText.swift
@@ -0,0 +1,96 @@
+//
+//  AppText.swift
+//  ICTMDBViewKit
+//
+//  Created by Engin Gülek on 24.12.2025.
+//
+
+import SwiftUI
+
+struct AppText: View {
+    enum Style {
+        case heroTitle
+        case sectionHeader
+        case cardTitle
+        case cardSubTitle
+        case body
+        case caption
+        case detailInfo
+        case castName
+        case castCharacter
+        case error
+    }
+    
+    let text: String
+    let style: Style
+    var color: Color? = nil
+    
+    var body: some View {
+        content
+            .foregroundColor(color ?? defaultColor)
+    }
+    
+    @ViewBuilder
+    private var content: some View {
+        switch style {
+        case .heroTitle:
+            Text(text)
+                .font(.system(size: 25, weight: .semibold))
+                .lineLimit(1)
+            
+        case .sectionHeader:
+            Text(text)
+                .font(.title2)
+                .fontWeight(.semibold)
+            
+        case .cardTitle:
+            Text(text)
+                .font(.system(size: 20, weight: .bold))
+                .lineLimit(2)
+            
+        case .cardSubTitle:
+            Text(text)
+                .font(.system(size: 20, weight: .semibold))
+            
+        case .body:
+            Text(text)
+                .font(.body)
+            
+        case .caption:
+            Text(text)
+                .font(.caption)
+                .fontWeight(.semibold)
+            
+        case .detailInfo:
+            Text(text)
+                .font(.system(size: 15, weight: .regular))
+                .lineLimit(7)
+            
+        case .castName:
+            Text(text)
+                .font(.system(size: 16, weight: .bold))
+                
+        case .castCharacter:
+            Text(text)
+                .font(.system(size: 16, weight: .bold))
+                
+        case .error:
+            Text(text)
+                .font(.headline)
+                .foregroundColor(.red)
+        }
+    }
+    
+    private var defaultColor: Color {
+        switch style {
+        case .heroTitle, .cardSubTitle, .detailInfo:
+            return .white
+        case .castCharacter:
+            return .gray
+        case .body:
+            return .primary.opacity(0.8)
+        default:
+            return .primary
+        }
+    }
+}
